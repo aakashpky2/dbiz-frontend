a6186c7c2c4309433e4d3321663ac79a7fe968b4
aakashpky2 <aakashpky2@gmail.com>
2026-07-31 11:03:42 +0530

    chore: fix robots configuration and remove static robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,10 @@
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/dashboard/', '/api/'],
-        },
-        sitemap: process.env.NEXT_PUBLIC_APP_URL
-            ? `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`
-            : 'https://dbizoffice.com/sitemap.xml',
-    };
+  return {
+    rules: {
+      userAgent: "*",
+      disallow: "/",
+    },
+  };
 }
